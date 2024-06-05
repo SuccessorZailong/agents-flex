ed724d3dac789bdaf8d357d6f130f709215291d3
zailong <1035381826@qq.com>
2024-06-05 16:58:34 +0800

    feat:Wenxin v1.0.0

--- agents-flex-llm/agents-flex-llm-wenxin/src/test/java/com/agentsflex/llm/wenxin/test/WenxinTest.java
+++ agents-flex-llm/agents-flex-llm-wenxin/src/test/java/com/agentsflex/llm/wenxin/test/WenxinTest.java
@@ -10,13 +10,13 @@ public class WenxinTest {
 
     public static void main(String[] args) throws InterruptedException {
         WenxinLlmConfig config = new WenxinLlmConfig();
-        config.setAppId("a8f7a50f-e0ad-40ea-aa8f-7f7ecbe73936");
-        config.setApiSecret("bce-v3/ALTAK-DfqCTSvkyn8OhrLE8PHgM/7d045be14b9a32c9ea459d401de7bf8ef5cbf6f0");
+        config.setAppId("753e8253-e7c2-4c88-aaf4-426225d49b6f");
+        config.setApiSecret("bce-v3/ALTAK-Q9ddySbqufKiYK0oy0LMj/f0c5d9ebc790d78fd5bd5e44aaf154cd4d6d804d");
 
 
         config.setDebug(true);
         Llm llm = new WenxinLlm(config);
-        llm.chatStream("请写一篇文章500-800字，关于牛生肖运势分析并送上祝福", (context, response) -> {
+        llm.chatStream("赛力斯是一家什么公司", (context, response) -> {
             AiMessage message = response.getMessage();
             System.out.println(">>>> " + message.getContent());
         });
